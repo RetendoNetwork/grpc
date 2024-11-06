361533238ea7dabee613f062685f7b7a7dfb5a50
cedkeChat <123301168+00cedke@users.noreply.github.com>
2024-11-06 18:44:32 +0100

    Update fix_imports.py

--- fix_imports.py
+++ fix_imports.py
@@ -1,7 +1,6 @@
 import os
 import re
 
-
 def replace_grpc_imports(directory):
     for root, _, files in os.walk(directory):
         for filename in files:
@@ -9,10 +8,17 @@ def replace_grpc_imports(directory):
                 file_path = os.path.join(root, filename)
                 with open(file_path, "r") as file:
                     content = file.read()
-                modified_content = re.sub(r"^(import.*pb2)", r"from . \1", content, flags=re.MULTILINE)
-                with open(file_path, "w") as file:
-                    file.write(modified_content)
-
+                
+                modified_content = re.sub(
+                    r"^(import .*\b(pb2|pb2_grpc)\b)",
+                    r"from . \1",
+                    content,
+                    flags=re.MULTILINE
+                )
+                
+                if modified_content != content:
+                    with open(file_path, "w") as file:
+                        file.write(modified_content)
 
 replace_grpc_imports("./account")
 replace_grpc_imports("./api")
